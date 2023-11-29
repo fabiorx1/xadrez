9188c31812109bb1664439bd97a745961e2b6ca3
fabiorx1 <binho-ramalho@hotmail.com>
2023-11-29 18:50:15 -0300

    wip: quadrado clicavel

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Xadrez dos Manitos',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Jogo de Xadrez'),
     );
   }
 }
@@ -27,6 +27,7 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+  final Color selectedColor = Colors.orange;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -39,6 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
     splashPath: 'peao.png',
     tipo: TipoPeca.peao,
   );
+  int? selected;
 
   @override
   void initState() {
--- lib/views/peca.dart
+++ lib/views/peca.dart
@@ -11,6 +11,7 @@ class QuadradoUI extends StatefulWidget {
   final void Function() onTap;
   final Color color;
   final Peca? p;
+  final bool selected = false;
 
   @override
   State<QuadradoUI> createState() => _QuadradoUIState();
@@ -27,29 +28,23 @@ class _QuadradoUIState extends State<QuadradoUI> {
     super.initState();
   }
 
-  void shineBright() => setState(() => color = colorBright);
-  void stayChill() => setState(() => color = widget.color);
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(2.0),
       child: GestureDetector(
         onTap: widget.onTap,
-        child: MouseRegion(
-            onEnter: (_) => shineBright(),
-            onExit: (_) => stayChill(),
-            child: Material(
-              color: color,
-              child: Center(
-                child: AspectRatio(
-                  aspectRatio: 0.5,
-                  child: widget.p != null
-                      ? Image.asset(widget.p!.splashPath)
-                      : const Center(),
-                ),
-              ), // here goes the peça icon
-            )),
+        child: Material(
+          color: color,
+          child: Center(
+            child: AspectRatio(
+              aspectRatio: 0.5,
+              child: widget.p != null
+                  ? Image.asset(widget.p!.splashPath)
+                  : const Center(),
+            ),
+          ), // here goes the peça icon
+        ),
       ),
     );
   }
